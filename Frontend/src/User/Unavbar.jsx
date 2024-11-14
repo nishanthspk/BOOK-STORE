4fea43d0a653db71c462ca4b96fbaee709042d9c
nishanthspk <spknishanth2004@gmail.com>
2024-11-14 11:51:07 +0530

    Unavbar

--- Frontend/src/User/Unavbar.jsx
+++ Frontend/src/User/Unavbar.jsx
@@ -9,16 +9,16 @@ const Unavbar = () => {
   return (
     <Navbar bg="" variant="dark" expand="lg" style={{backgroundColor:"blue"}}>
       <Container>
-        <Navbar.Brand ><Link to='/uhome' style={{color:'white',textDecoration:"none"}}>BookStore</Link></Navbar.Brand>
+        <Navbar.Brand ><Link to='/uhome' className='fs-2' style={{color:'white',textDecoration:"none"}}>BookStore</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto" >
-            <Link to="/uhome" style={{padding:"10px",color:"white",textDecoration:"none"}}>Home</Link>
-            <Link to="/uproducts" style={{padding:"10px",color:"white",textDecoration:"none"}}>Books</Link>
-            <Link to="/wishlist" style={{padding:"10px",color:"white",textDecoration:"none"}}>Wishlist</Link>
-            <Link to="/myorders" style={{padding:"10px",color:"white",textDecoration:"none"}}>My orders</Link>
-            <Link to="/" style={{paddingLeft:"10px",paddingTop:"10px",color:"white",textDecoration:"none"}}>Logout</Link>
-            <h4 style={{color:"white",paddingTop:"0px"}}>({JSON.parse(get).name} )</h4>
+            <Link to="/uhome" className='fs-4' style={{padding:"10px",color:"white",textDecoration:"none"}}>Home</Link>
+            <Link to="/uproducts" className='fs-4' style={{padding:"10px",color:"white",textDecoration:"none"}}>Books</Link>
+            <Link to="/wishlist" className='fs-4' style={{padding:"10px",color:"white",textDecoration:"none"}}>Wishlist</Link>
+            <Link to="/myorders" className='fs-4' style={{padding:"10px",color:"white",textDecoration:"none"}}>My orders</Link>
+            <Link to="/" className='fs-4' style={{paddingLeft:"10px",paddingTop:"10px",color:"white",textDecoration:"none"}}>Logout</Link>
+            <h4 style={{color:"white",paddingTop:"14px",paddingLeft:"10px"}}>({JSON.parse(get).name} )</h4>
           </Nav>
         </Navbar.Collapse>
       </Container>
