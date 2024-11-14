14a4f2ade599ef21893aef564f9419fd9a7b9f8b
nishanthspk <spknishanth2004@gmail.com>
2024-11-14 12:43:58 +0530

    BookStore

--- Frontend/src/Componenets/Home.jsx
+++ Frontend/src/Componenets/Home.jsx
@@ -10,15 +10,15 @@ const Home = () => {
   return (
     <div className='' style={{ backgroundImage: `url(${BG})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh', width: '100vw' }}>
     
-    <Navbar bg="" variant="dark" expand="lg" style={{backgroundColor:"brown" , height: '10vh'}}>
+    <Navbar bg="" variant="dark" expand="lg" style={{backgroundColor:"transparent" , height: '10vh'}}>
       <Container>
-        <Navbar.Brand className="fs-1"><Link to='/' style={{color:'white',textDecoration:"none"}}>BookStore</Link></Navbar.Brand>
+        <Navbar.Brand className="fs-1" style={{paddingTop:"20px"}}><Link to='/' style={{color:'white',textDecoration:"none"}}>Tales & Tomes</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto fs-2">
-            <Link to="/login" style={{padding:"10px",color:"white",textDecoration:"none"}}>User</Link>
-            <Link to="/slogin" style={{padding:"10px",color:"white",textDecoration:"none"}}>Seller</Link>
-            <Link to="/alogin" style={{padding:"10px",color:"white",textDecoration:"none"}}>Admin</Link>   
+            <Link to="/login" style={{padding:"px",color:"white",textDecoration:"none",paddingTop:"30px",backgroundColor: ''}}>User</Link>
+            <Link to="/slogin" style={{padding:"10px",color:"white",textDecoration:"none",paddingTop:"30px"}}>Seller</Link>
+            <Link to="/alogin" style={{padding:"10px",color:"white",textDecoration:"none",paddingTop:"30px"}}>Admin</Link>   
           </Nav>
         </Navbar.Collapse>
       </Container>
