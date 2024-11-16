d1a98240fc7ff84aa789f593d5219551a5fd8865
nishanthspk <spknishanth2004@gmail.com>
2024-11-16 12:29:10 +0530

    Document

--- Frontend/src/Componenets/Home.jsx
+++ Frontend/src/Componenets/Home.jsx
@@ -2,30 +2,30 @@
 
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import BG from "../Assets/Bookbg.jpg";
 
 const Home = () => {
- 
+
   return (
     <div className='' style={{ backgroundImage: `url(${BG})`, backgroundSize: 'cover', backgroundPosition: 'center', height: '100vh', width: '100vw' }}>
-    
-    <Navbar bg="" variant="dark" expand="lg" style={{backgroundColor:"transparent" , height: '10vh'}}>
-      <Container>
-        <Navbar.Brand className="fs-1" style={{paddingTop:"20px"}}><Link to='/' style={{color:'white',textDecoration:"none"}}>Tales & Tomes</Link></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto fs-2">
-            <Link to="/login" className="rounded-2xl text-black bg-blue-100" style={{padding:"6px",color:"white",textDecoration:"none",marginTop:"30px",marginRight:"10px",backgroundColor: ''}}>User</Link>
-            <Link to="/slogin"  className="rounded-2xl text-black bg-blue-100" style={{padding:"6px",color:"white",textDecoration:"none",marginTop:"30px",marginRight:"10px",backgroundColor: ''}}>Seller</Link>
-            <Link to="/alogin"  className="rounded-2xl text-black bg-blue-100" style={{padding:"6px",color:"white",textDecoration:"none",marginTop:"30px",backgroundColor: ''}}>Admin</Link>   
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    
+
+      <Navbar bg="" variant="dark" expand="lg" style={{ backgroundColor: "transparent", height: '10vh' }}>
+        <Container>
+          <Navbar.Brand className="fs-1" style={{ paddingTop: "20px" }}><Link to='/' style={{ color: 'white', textDecoration: "none" }}>Tales & Tomes</Link></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto fs-2">
+              <Link to="/login" className="rounded-2xl text-black bg-blue-100" style={{ padding: "6px", color: "white", textDecoration: "none", marginTop: "30px", marginRight: "10px", backgroundColor: '' }}>User</Link>
+              <Link to="/slogin" className="rounded-2xl text-black bg-blue-100" style={{ padding: "6px", color: "white", textDecoration: "none", marginTop: "30px", marginRight: "10px", backgroundColor: '' }}>Seller</Link>
+              <Link to="/alogin" className="rounded-2xl text-black bg-blue-100" style={{ padding: "6px", color: "white", textDecoration: "none", marginTop: "30px", backgroundColor: '' }}>Admin</Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
     </div>
-  
+
   );
 };
 
