6abfef3c9533112edfd73e674621d8bb5bd9426c
nishanthspk <spknishanth2004@gmail.com>
2024-11-16 12:26:55 +0530

    Admin

--- Frontend/src/Admin/Ahome.jsx
+++ Frontend/src/Admin/Ahome.jsx
@@ -38,14 +38,14 @@
 //     { name: 'Users', value: totalUsers },
 //     { name: 'Bookings', value: totalBookings },
 //   ];
-  
+
 //   // Define colors for the chart
 //   const colors = [ 'green','#2B124C', ];
 
 //   return (
 //     <div>
 //       <h1 className="text-center">Users and Bookings Pie Chart</h1>
-      
+
 //       <Card body style={{ background: "white", width: "80%", marginLeft: "17%", marginTop: "20px", height: "650px" }}>
 //         <div className="flex justify-around items-center p-4">
 //           <Link to="/bookings" style={{textDecoration:"none"}}>
@@ -60,7 +60,7 @@
 //           </Link>
 //         </div>
 //         <Card>
-        
+
 //         <PieChart width={400} height={400}>
 //         <Pie
 //           dataKey="value"
@@ -71,7 +71,7 @@
 //           outerRadius={80}
 //           fill="#8884d8"
 //           label >
-        
+
 //           {data.map((entry,index)=>(
 //              <Cell key={`cell-${index}`} fill={colors[index]} />
 //           ))}
@@ -81,7 +81,7 @@
 //       </PieChart>
 //         </Card>
 //       </Card>
-     
+
 //     </div>
 //   );
 // }
@@ -97,7 +97,7 @@ import Anavbar from './Anavbar';
 
 function Ahome() {
   const [users, setUsers] = useState([]);
-  const [vendors,setVendors]=useState([])
+  const [vendors, setVendors] = useState([])
   const [items, setItems] = useState([]);
   const [orders, setOrders] = useState([]);
 
@@ -120,26 +120,26 @@ function Ahome() {
         console.error('Error fetching bookings: ', error);
       });
 
-      // Fetch items data
+    // Fetch items data
     axios.get(`http://localhost:4000/item`)
-    .then((response) => {
-      setItems(response.data);
-    })
-    .catch((error) => {
-      console.error('Error fetching bookings: ', error);
-    });
+      .then((response) => {
+        setItems(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching bookings: ', error);
+      });
 
-      // Fetch orders data
+    // Fetch orders data
     axios.get(`http://localhost:4000/orders`)
-    .then((response) => {
-      setOrders(response.data);
-    })
-    .catch((error) => {
-      console.error('Error fetching bookings: ', error);
-    });
+      .then((response) => {
+        setOrders(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching bookings: ', error);
+      });
   }, []);
- 
-  const colors = ['#2B124C', '#AE4451', '#F39F5A','orange'];
+
+  const colors = ['#2B124C', '#AE4451', '#F39F5A', 'orange'];
 
   // Calculate the number of users and bookings
   const totalUsers = users.length;
@@ -149,53 +149,53 @@ function Ahome() {
 
   // Define data for the bar chart
   const data = [
-    { name: 'Users', value: totalUsers, fill: '#2B124C' }, 
+    { name: 'Users', value: totalUsers, fill: '#2B124C' },
     { name: 'vendors', value: totalvendors, fill: 'cyan' },
-    { name: 'Items', value: totalItems, fill: 'blue' }, 
-    { name: 'Orders', value: totalOrders, fill: 'orange' }, 
+    { name: 'Items', value: totalItems, fill: 'blue' },
+    { name: 'Orders', value: totalOrders, fill: 'orange' },
   ];
   return (
-    <div className=''style={{backgroundColor:"#81b5fa",paddingBottom:"210px"}} >
-        <Anavbar/>
-      <h3 className="text-center pt-20" style={{color:""}}>DashBoard</h3>
+    <div className='' style={{ backgroundColor: "#81b5fa", paddingBottom: "210px" }} >
+      <Anavbar />
+      <h3 className="text-center pt-20" style={{ color: "" }}>DashBoard</h3>
       <Card body style={{ background: "wheat", width: "80%", marginLeft: "10%", marginTop: "20px", height: "580px" }}>
         <div className="flex justify-around items-center p-4">
-           <Link to="/users" style={{textDecoration:"none"}}>
-          <div className="w-64 h-32 bg-red-500 rounded-lg shadow-md flex flex-col justify-center items-center text-xl font-bold text-gray-800 text-center">
-           USERS <br /> <br />{totalUsers}
-         </div>
-         </Link> 
-        <Link to="/vendors" style={{textDecoration:"none"}}>
-         <div className="w-64 h-32 bg-blue-500 rounded-lg shadow-md flex flex-col justify-center items-center text-xl font-bold text-gray-800 text-center">
-           Vendors <br /> <br /> {totalvendors}
-         </div>
-         </Link>
-         <Link to="/vendors" style={{textDecoration:"none"}}>
-          <div className="w-64 h-32 bg-green-500 rounded-lg shadow-md flex flex-col justify-center items-center text-xl font-bold text-gray-800 text-center">
-           Items <br /> <br />{totalItems}
-         </div>
-         </Link>
-         <Link to="/users" style={{textDecoration:"none"}}>
-          <div className="w-64 h-32 bg-yellow-500 rounded-lg shadow-md flex flex-col justify-center items-center text-xl font-bold text-gray-800 text-center">
-           Total Orders <br /> <br />{totalOrders}
-         </div>
-         </Link>
-       </div>
-       <br/>
-       <br/>
-       <br/>
-       <div style={{paddingLeft:"350px"}}>
-       <BarChart width={400} height={300} data={data} >
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip   />
-          <Legend />
-          <Bar dataKey="value" fill="#8884d8" barSize={50} />
-          
-        </BarChart>
-       </div>
-       </Card>
-   
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <div className="w-64 h-32 bg-red-500 rounded-lg shadow-md flex flex-col justify-center items-center text-xl font-bold text-gray-800 text-center">
+              USERS <br /> <br />{totalUsers}
+            </div>
+          </Link>
+          <Link to="/vendors" style={{ textDecoration: "none" }}>
+            <div className="w-64 h-32 bg-blue-500 rounded-lg shadow-md flex flex-col justify-center items-center text-xl font-bold text-gray-800 text-center">
+              Vendors <br /> <br /> {totalvendors}
+            </div>
+          </Link>
+          <Link to="/vendors" style={{ textDecoration: "none" }}>
+            <div className="w-64 h-32 bg-green-500 rounded-lg shadow-md flex flex-col justify-center items-center text-xl font-bold text-gray-800 text-center">
+              Items <br /> <br />{totalItems}
+            </div>
+          </Link>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <div className="w-64 h-32 bg-yellow-500 rounded-lg shadow-md flex flex-col justify-center items-center text-xl font-bold text-gray-800 text-center">
+              Total Orders <br /> <br />{totalOrders}
+            </div>
+          </Link>
+        </div>
+        <br />
+        <br />
+        <br />
+        <div style={{ paddingLeft: "350px" }}>
+          <BarChart width={400} height={300} data={data} >
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="value" fill="#8884d8" barSize={50} />
+
+          </BarChart>
+        </div>
+      </Card>
+
     </div>
   );
 }
